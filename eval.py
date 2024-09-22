16d76d5d3778d776cd0527f53909b0e9e809115e
Aakarsh <aakarshgoel96@gmail.com>
2024-09-22 11:34:38 +0200

    Evaluation code added eval.py

---
+++ eval.py
@@ -0,0 +1,23 @@
+import argparse
+from ultralytics import YOLO
+from utils import visualize_results, prepare_data_yaml
+
+def evaluate(model_path, data_path):
+    # Prepare the data.yaml file
+    prepare_data_yaml(data_path)
+    
+    model = YOLO(model_path)
+    
+    # Evaluate the model
+    results = model.val(data=data_path)
+    
+    # Visualize evaluation results
+    visualize_results(results)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Evaluate YOLOv8 for vehicle damage detection')
+    parser.add_argument('--model', type=str, required=True, help='Path to trained model')
+    parser.add_argument('--data', type=str, default='/app/data/data.yaml', help='Path to data.yaml file')
+    args = parser.parse_args()
+
+    evaluate(args.model, args.data)
